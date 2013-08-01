57f300ffaba1643ec5867dc8bfdd31fe6847f9a9
Luke Ross <luke@conversocial.com>
2013-08-01 13:23:34 +0100

    Third user for checking privacy.

--- create_test_users.py
+++ create_test_users.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python 
+
 from datetime import datetime
 
 import pyaspora.tools.sqlalchemy
@@ -16,7 +18,6 @@ u.contact.public_key = ''
 u.private_key = ''
 u.email = 'notluke@example.com'
 u.activated = datetime.now()
-u.set_password('test')
 session.add(u)
 session.commit()
 u = model.User.get_by_email(u.email)
@@ -32,7 +33,21 @@ u.contact.public_key = ''
 u.private_key = ''
 u.email = 'luke@example.com'
 u.activated = datetime.now()
-u.set_password('test')
+session.add(u)
+session.commit()
+u = model.User.get_by_email(u.email)
+u.generate_keypair('test')
+session.add(u.contact)
+session.add(u)
+session.commit()
+
+u = model.User()
+u.contact.realname = 'Nosy Nosy'
+u.contact.username = 'nosy@localhost'
+u.contact.public_key = ''
+u.private_key = ''
+u.email = 'nosy@example.com'
+u.activated = datetime.now()
 session.add(u)
 session.commit()
 u = model.User.get_by_email(u.email)
