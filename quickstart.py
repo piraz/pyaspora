086ccc63361cc77d5866fcb3c91322cf55ddedbd
Luke Ross <luke@lukeross.name>
2014-03-04 22:16:27 +0000

    New config vars.

--- quickstart.py
+++ quickstart.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python
 
-import os
 from pyaspora import app
-app.secret_key = os.urandom(24)
+
+# Command to generate random string:
+# python -c 'import os; import base64; print(base64.b64encode(os.urandom(32)))'
+app.secret_key = None # Use a random string (see comment above)
+
+# These control sending of email - who to send as, and using which server
+app.config['SMTP_FROM'] = None # 'address@example.com'
+app.config['SMTP_URL'] = None # 'smtp://smtp.example.com/'
+
+# You can change the database used here
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../database.sqlite'
+
+# This controls where uploaded files are placed temporarily
 app.config['UPLOAD_FOLDER'] = '/tmp'
+
 app.run(debug=True)
