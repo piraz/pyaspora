16359c25bf8ebb362db12476d73fe4356c25ce51
Luke Ross <luke@lukeross.name>
2014-03-16 19:39:48 +0000

    Update the params.

--- quickstart.py
+++ quickstart.py
@@ -4,7 +4,8 @@ from pyaspora import app
 
 # Command to generate random string:
 # python -c 'import os; import base64; print(base64.b64encode(os.urandom(32)))'
-app.secret_key = None # Use a random string (see comment above)
+app.secret_key = None # Use a random string (see comment above). This should
+                      # kept secret to keep your sessions secure.
 
 # These control sending of email - who to send as, and using which server
 app.config['SMTP_FROM'] = None # 'address@example.com'
@@ -16,7 +17,13 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../database.sqlite'
 # This controls where uploaded files are placed temporarily
 app.config['UPLOAD_FOLDER'] = '/tmp'
 
-assert app.secret_key and ('SMTP_FROM', 'SMTP_URL') in app.config, \
+# Whether to allow new-user signup
+app.config['ALLOW_CREATION'] = False
+
+# Whether to permit user download from HTTP (not HTTPS)
+app.config['ALLOW_INSECURE_HOSTMETA'] = False
+
+assert app.secret_key, \
     'You need to edit quickstart.py to configure the application'
 
 app.run(debug=True)
